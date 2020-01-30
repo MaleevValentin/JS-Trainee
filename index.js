fabbd4e0707c5409503e5c4c2191615081e5cf59
Valentin <maleyev.valentin@gmail.com>
2020-01-30 17:17:05 +0200

    решены задачи по игре Виселица

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ var word = words[Math.floor(Math.random() * words.length)];
 
 //Создаем итоговый массив
 var answerArray = [];
-for(var i = 0; i < word.length; i++) {
+for (var i = 0; i < word.length; i++) {
   answerArray[i] = "_";
 }
 
 var remainingLetters = word.length;
+var remainingTimes = 10;
 
 //Игровой цикл
-while (remainingLetters > 0) {
+while (remainingLetters > 0 && remainingTimes > 0) {
   //Показываем состояние игры
   alert(answerArray.join(" "));
   //Запрашиваем вариант ответа
@@ -31,19 +32,26 @@ while (remainingLetters > 0) {
     //Выходим из игрового цикла
     break;
   } else if (guess.length !== 1) {
-    alert ("Пожалуйста, введите одну букву");
+    remainingTimes--;
+    alert("Пожалуйста, введите одну букву");
   } else {
     //Обновляем состояние игры
+    remainingTimes--;
     for (var j = 0; j < word.length; j++) {
-      if (word[j] === guess.toLowerCase()) {
+      if (word[j] === guess.toLowerCase() && answerArray[j] === "_") {
         answerArray[j] = guess.toLowerCase();
         remainingLetters--;
+        }
       }
     }
-  }
   //Конец игрового цикла
 }
 
 //Отображаем ответ и поздравляем игрока
 alert(answerArray.join(" "));
-alert("Отлично. Было загадано слово: " + word);
+if (remainingTimes >= 0) {
+  alert("Отлично. Было загадано слово: " + word);
+} else {
+  alert("Плохо! Было загадано слово: " + word);
+}
+
