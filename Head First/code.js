05e3b5f02de73a406d8135f530073fcac48f709b
Valentin <maleyev.valentin@gmail.com>
2020-03-03 23:49:47 +0200

    objects final chapter 5

--- Head First/code.js
+++ Head First/code.js
@@ -1,13 +1,18 @@
-let word = "bottles";
-let count = 99;
-while (count > 0) {
-  console.log(count + " " + word + " of beer on the wall");
-  console.log(count + " " + word + " of beer,");
-  console.log("Take one down, pass it around,");
-  count -= 1;
-  if (count > 0) {
-    console.log(count + " " + word + " of beer on the wall.");
-  } else {
-    console.log("No more " + word + " of beer on the wall.");
-  }
-}
+// let word = "bottles";
+// let count = 99;
+// while (count > 0) {
+//   console.log(count + " " + word + " of beer on the wall");
+//   console.log(count + " " + word + " of beer,");
+//   console.log("Take one down, pass it around,");
+//   count -= 1;
+//   if (count > 0) {
+//     console.log(count + " " + word + " of beer on the wall.");
+//   } else {
+//     console.log("No more " + word + " of beer on the wall.");
+//   }
+// }
+
+let access = document.getElementById("code9");
+let code = access.innerHTML;
+code = code + " midnight";
+alert(code);
