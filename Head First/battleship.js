2da7eb456ed9f79359d3a5b77bca6d6099d4a4ea
Valentin <maleyev.valentin@gmail.com>
2020-03-10 23:03:45 +0200

    battleship model

--- Head First/battleship.js
+++ Head First/battleship.js
@@ -13,10 +13,41 @@ let view = {
   }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Forza Juve!!!")
+let model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] }
+  ],
+  fire: function(guess) {
+    for (let i = 0; i < this.numShips; i++) {
+      let ship = this.ships[i];
+      let index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!!!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!!!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed!!!");
+    return false;
+  },
+  isSunk: function(ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
+};
